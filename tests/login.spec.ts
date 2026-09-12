02209bd8f31bf860e0293989cffcabe3eb97de0c
ankush <ankush@azafashions.com>
2026-09-12 16:10:12 +0530

    add login page object fixture

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,24 +1,20 @@
-import { test, expect } from "@playwright/test";
-import { LoginPage } from "@pages/login.page";
+import { test, expect } from "@fixtures/page.fixture";
 import { validUser, invalidUser } from "@test-data/users";
-
-let loginPage: LoginPage;
 
 test.describe("Login Tests", () => {
 
     test.beforeEach(async ({ page }) => {
-        loginPage = new LoginPage(page);
         await page.goto("/");
-    })
+    });
 
-    test("Verify login with valid credentials", async ({ page }) => {
+    test("Verify login with valid credentials", async ({ page, loginPage }) => {
         await loginPage.login(validUser.username, validUser.password)
         await expect(page).toHaveURL(/inventory.html/);
-    })
+    });
 
-    test("Verify login with invalid credentials", async ({ page }) => {
+    test("Verify login with invalid credentials", async ({ loginPage }) => {
         await loginPage.login(invalidUser.username, invalidUser.password);
         await loginPage.expectLoginError("Epic sadface: Username and password do not match any user in this service");
-    })
+    });
 
-})
+});
