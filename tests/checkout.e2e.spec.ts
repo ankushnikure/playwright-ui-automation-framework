45f437f67ca6e896db8a9b5259bc3aa07a923464
ankush <ankush@azafashions.com>
2026-09-18 13:07:35 +0530

    add test for Jenkins failure debugging

--- tests/checkout.e2e.spec.ts
+++ tests/checkout.e2e.spec.ts
@@ -45,13 +45,13 @@ test("Verify user can complete checkout and download order PDF @smoke", async ({
     await test.step("Continue checkout", async () => {
         await checkoutInformationPage.continueCheckout();
         expect(checkoutOverviewPage.getCurrentUrl()).toContain("checkout-step-two.html");
-        await expect(checkoutOverviewPage.pageTitle).toHaveText("Checkout: Overview");
+        expect(checkoutOverviewPage.pageTitle).toHaveText("Checkout: Overview");
     });
 
     await test.step("Complete checkout", async () => {
         await checkoutOverviewPage.finishCheckout();
         expect(checkoutCompletePage.getCurrentUrl()).toContain("checkout-complete.html");
-        await expect(checkoutCompletePage.pageTitle).toHaveText("Checkout: Complete!");
+        expect(checkoutCompletePage.pageTitle).toHaveText("Checkout: Complete!");
     });
 
     await test.step("Download order PDF", async () => {
