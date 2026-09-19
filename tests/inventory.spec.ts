7082af3cfc7e5f4abeeaa531c3e0d24c7ec3dfb0
ankush <ankush@azafashions.com>
2026-09-19 12:54:14 +0530

    add test failure to smoke suite

--- tests/inventory.spec.ts
+++ tests/inventory.spec.ts
@@ -9,7 +9,7 @@ test.describe("Inventory tests", () => {
         await loginPage.login(validUser.username, validUser.password);
     });
 
-    test("Verify user can add specific product to cart", async ({ inventoryPage }) => {
+    test("Verify user can add specific product to cart @smoke", async ({ inventoryPage }) => {
         expect(inventoryPage.getCurrentUrl).toContain("inventory.html");
         await inventoryPage.addProductToCart(products.backpack)
         expect(await inventoryPage.isRemoveButtonVisible(products.backpack)).toBeTruthy();
